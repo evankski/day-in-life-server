ea449b1ecedced835b31a77652a00b81ca4c2a0a
danieldenton <danielmdenton@gmail.com>
2022-03-25 11:29:40 -0700

    updated user.js model

--- models/user.js
+++ models/user.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
+const CommentSchema = new mongoose.Schema(
+  {
+    name: String,
+    content: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PhotoSchema = new mongoose.Schema(
+  {
+    url: String,
+    caption: String,
+    comments: [CommentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  profile_url: String,
+  photo: [PhotoSchema],
 });
 
 module.exports = mongoose.model("User", UserSchema);
