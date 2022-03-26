bf59de1949ca227167bf221438d29e1734d9c624
danieldenton <danielmdenton@gmail.com>
2022-03-25 20:31:18 -0700

    pic-controller get put delete incomplete

--- controllers/api-v1/pictures.js
+++ controllers/api-v1/pictures.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const requiresToken = require("./requiresToken");
 
 const cloudinary = require("cloudinary").v2;
 
-const { unLinkSync } = require("fs");
+const { unlinkSync } = require("fs");
+const { db } = require("../../models/user");
 
 const newPics = multer({ dest: "uploads/" });
 
-router.post("/", newPics.single("image"), async (req, res) => {
+router.post("/", newPics.single("image"), requiresToken, async (req, res) => {
   try {
     if (!req.file) return res.status(400).json({ msg: "no file uploaded" });
     const cloudinaryImageData = await cloudinary.uploader.upload(req.file.path);
@@ -16,46 +18,47 @@ router.post("/", newPics.single("image"), async (req, res) => {
     const foundUser = res.locals.user;
     // const cloudImage = `https://res.cloudinary.com/dhs1wrqhp/image/upload/v1593119998/${cloudImageData.public_id}.png`;
     foundUser.photos.push({
-      public_id: cloudImageData.public_id,
+      public_id: cloudinaryImageData.public_id,
       caption: req.body.caption,
     });
     await foundUser.save();
-    unLinkSync(req.file.path);
+    unlinkSync(req.file.path);
     // res.json({ cloudImage });
     res.status(201).json({ msg: "image posted to db" });
   } catch (err) {
-    console.log(
-      err.status(503).json({ msg: "you should look at the server console" })
-    );
+    console.log(err);
+    res.status(503).json({ msg: "you should look at the server console" });
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    console.log(await db.photo.findOne());
-    const photos = await db.photo.findOne({
-      //   url:
-    });
-    res.json({ cloudImage });
+    const photId = await db.user.photos.public_id;
+    const cloudImage = `https://res.cloudinary.com/dhs1wrqhp/image/upload/v1593119998/${photId}.png`;
+    res.status(200).json({ cloudImage });
   } catch (err) {
     console.log(err);
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", requiresToken, async (req, res) => {
   try {
-    console.log(await db.photo.findOne());
-    const pictures = await db.photo.caption.updateOne({});
-    res.json({ cloudImage });
+    if (req.body.user_id === res.locals.user.id) {
+      const photo = await db.users.photos.findOneAndUpdate({
+        caption: req.body.caption,
+      });
+    }
+    res.status(202).json({ msg: "the caption has been updated" });
   } catch (err) {
     console.log(err);
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", requiresToken, async (req, res) => {
   try {
-    console.log(await db.photo.findOne());
-    const pictures = await db.photo.remove({});
+    if (req.body.user_id === res.locals.user.id) {
+      const photo = await db.users.photos.remove({});
+    }
   } catch (err) {
     console.log(err);
   }
