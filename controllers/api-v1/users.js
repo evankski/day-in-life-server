3626a6b73bd0e1c7446747865c46962a11c8207d
danieldenton <danielmdenton@gmail.com>
2022-03-31 05:59:39 -0700

    delete user route

--- controllers/api-v1/users.js
+++ controllers/api-v1/users.js
@@ -111,6 +111,7 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// PUT /users/:id -- UPDATE user profile pic
 router.put("/:id", newPics.single("image"), requiresToken, async (req, res) => {
   try {
     if (!req.file) return res.status(400).json({ msg: "no file uploaded" });
@@ -124,7 +125,21 @@ router.put("/:id", newPics.single("image"), requiresToken, async (req, res) => {
     res.status(201).json({ msg: "upload success" });
   } catch (err) {
     console.log(err);
-    res.status(503).json({ msg: "you should look at the server console" });
+    res.status(503).json({ msg: "server error" });
+  }
+});
+
+// DELETE /users/:id -- DESTROY user profile with :id and its subdocs
+router.delete("/:id", requiresToken, async (req, res) => {
+  try {
+    const foundUser = await db.User.findOne({
+      "users._id": req.params.id,
+    });
+    await foundUser.remove();
+    res.status(200).json({ msg: "this user has been deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(503).json({ msg: "server error" });
   }
 });
 
